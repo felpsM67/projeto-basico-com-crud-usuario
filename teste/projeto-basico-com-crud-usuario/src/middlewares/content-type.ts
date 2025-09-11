1be8d57baa71a6088fdbbbcc80a5b3a37513bb3c
jhonata sobral <jhonata.sobral@ms.senac.br>
2025-09-11 19:52:35 -0400

    altera documentação do swagger

--- teste/projeto-basico-com-crud-usuario/src/middlewares/content-type.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-const contentType = (req: Request, res: Response, next: NextFunction) => {
-  res.type('json');
-  next();
-};
-
-export default contentType;
