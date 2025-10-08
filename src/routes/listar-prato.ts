5752b2696b7370d5e715d766b9215053cba485e6
jhonata sobral <jhonata.sobral@ms.senac.br>
2025-10-08 19:57:51 -0400

    ajustes de rotas

--- src/routes/listar-prato.ts
+++ src/routes/listar-prato.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import adaptRoute from "../adapters/express-route-adapter";
 import ListarPratoController from "../controllers/prato/listar-prato";
-import { authMiddleware, authorizeRoles } from "../middlewares";
 
 export default (router: Router): void => {
   router.get("/pratos/{:id}",
