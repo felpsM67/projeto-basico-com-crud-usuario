c4e45bb6ba4d9838837ce8710f4cf18c2058c6b7
jhonata sobral <jhonata.sobral@ms.senac.br>
2025-10-13 19:33:48 -0400

    ajustes de controller criar usuário

--- src/controllers/usuario/criar-usuario.ts
+++ src/controllers/usuario/criar-usuario.ts
@@ -1,58 +1,26 @@
-import { Role } from "../../enums/role";
-import Cliente from "../../models/cliente-model";
-import User from "../../models/user-model";
-import bcrypt from "bcrypt";
-import validator from "validator";
-import Funcionario from "../../models/funcionario-model";
-import Gerente from "../../models/gerente-model";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 import { UsuarioService } from "../../service/usuario-service";
-import { created, serverError } from "../../helpers/http-helper";
+import { badRequest, created, serverError } from "../../helpers/http-helper";
+import { InvalidParamError } from "../../errors";
 class CriarUsuarioController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { nome, email, senha, role } = httpRequest.body;
       const usuarioService = new UsuarioService();
       const user = await usuarioService.buscarPorEmail( email);
-
       if (user) {
-        return {
-          statusCode: 400,
-          body: { error: "Email já cadastrado" },
-        };
+        return badRequest(new InvalidParamError("email"));
       }
-
       const usuario = await usuarioService.criarUsuario({
         nome,
         email,
         senha,
         role,
       });
-
       return created(usuario);
     } catch (error: any) {
       return serverError();
     }
   }
-
-  async criarPerfil(userId: number, role: string, nome: string) {
-    if (role === Role.CLIENTE) {
-      await Cliente.create({
-        nome,
-        userId,
-      });
-    } else if (role === Role.FUNCIONARIO) {
-      await Funcionario.create({
-        nome,
-        userId,
-      });
-    } else if (role === Role.GERENTE) {
-      await Gerente.create({
-        nome,
-        userId,
-      });
-    }
-  }
 }
-
 export default CriarUsuarioController;
