5d9ff3ee6ce273c5ca75f3c428eec22a6a27dd28
jhonata sobral <jhonata.sobral@ms.senac.br>
2025-10-20 21:17:04 -0400

    ajustes de rota

--- src/routes/listar-prato.ts
+++ src/routes/listar-prato.ts
@@ -3,5 +3,5 @@ import adaptRoute from "../adapters/express-route-adapter";
 import ListarPratoController from "../controllers/prato/listar-prato";
 
 export default (router: Router): void => {
-  router.get("/pratos/{:id}", adaptRoute(new ListarPratoController()));
+  router.get("/pratos{/:id}", adaptRoute(new ListarPratoController()));
 };
