d30d5240c3cf817cda7530a99d80fb1bed75bdf2
Jhonata Sobral <sobral.jhonata@gmail.com>
2025-10-27 16:40:29 -0400

    ajustes de suite de testes

--- test/setup-env.ts
+++
@@ -1,8 +0,0 @@
-// test/setup-env.ts
-import dotenv from "dotenv";
-
-// Carrega as variáveis de ambiente do arquivo .env
-dotenv.config({ path: '.env.test' });
-
-// Garante um ambiente consistente
-process.env.NODE_ENV = process.env.NODE_ENV || 'test';
