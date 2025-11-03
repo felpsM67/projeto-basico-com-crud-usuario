415e97e4da0812c7c35c1fc52cfe7be04d113104
jhonata sobral <jhonata.sobral@ms.senac.br>
2025-11-03 19:05:22 -0400

    ajustes na rota de listar prato

--- src/controllers/prato/listar-prato.ts
+++ src/controllers/prato/listar-prato.ts
@@ -1,10 +1,17 @@
-import { ok, serverError } from "../../helpers/http-helper";
+import { notFound, ok, serverError } from "../../helpers/http-helper";
 import Prato from "../../models/prato-model";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 
 export default class ListarPratoController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const pratoId = httpRequest.params.id;
+      const prato = await Prato.findByPk(pratoId);
+      if (!prato && pratoId !== "{id}" && pratoId !== undefined) {
+        return notFound({ error: "Prato não encontrado" });
+      } else if (pratoId !== "{id}" && pratoId !== undefined) {
+        return ok(prato);
+      }
       const pratos = await Prato.findAll();
       return ok(pratos);
     } catch (error: any) {
